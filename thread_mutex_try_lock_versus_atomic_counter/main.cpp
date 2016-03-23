2dfeb313a2d96d5a67441d2803369b6e6c83cc47
Patryk Małek <malekpatryk@gmail.com>
2016-03-23 11:59:01 +0100

    Modified thread_mutex_try_lock_versus_atomic_counter

--- thread_mutex_try_lock_versus_atomic_counter/main.cpp
+++ thread_mutex_try_lock_versus_atomic_counter/main.cpp
@@ -3,27 +3,22 @@
 #include <mutex>          // std::mutex
 #include <chrono>
 #include <atomic>
+#include <vector>
 
 std::atomic<u_int64_t> counter_atomic (0); // atomic counter
 u_int64_t              counter        (0); // counter locked with mutex
-std::mutex mtx;                            // locks access to counter
+std::mutex             mtx;                // locks access to counter
 u_int64_t              unsync_counter (0); // non-atomic counter
 
 void attempt_10k_increases_mutex () {
   for (u_int32_t i=0; i<1000000; ++i) {
-    while (!mtx.try_lock()) {   // only increase if currently not locked
-      ++unsync_counter;
-    }
-      ++counter;
-      mtx.unlock();
+    std::lock_guard<std::mutex> lock(mtx);
+    ++counter;
   }
 }
 
 void attempt_10k_increases_atomic () {
-  for (u_int32_t i=0; i<1000000; ++i) {
-      ++counter_atomic;
-      mtx.unlock();
-  }
+  for (u_int32_t i=0; i<1000000; ++i) ++counter_atomic;
 }
 
 double p(std::chrono::duration<double, std::milli> duration)
@@ -33,20 +28,21 @@ double p(std::chrono::duration<double, std::milli> duration)
 
 int main ()
 {
-  constexpr uint8_t num = 5;
+  const uint8_t num = std::thread::hardware_concurrency();
 
   auto start_atomic = std::chrono::steady_clock::now();
-  std::thread threads_atomic[num];
-  for (int i=0; i<num; ++i)
-    threads_atomic[i] = std::thread(attempt_10k_increases_atomic);
+
+  std::vector<std::thread> threads_atomic;
+  threads_atomic.reserve(num);
+  for (int i=0; i<num; ++i) threads_atomic.emplace_back(attempt_10k_increases_atomic);
   for (auto& th : threads_atomic) th.join();
   std::cout << "atomic version took "
             << p(std::chrono::steady_clock::now() - start_atomic) << "ms\n";
 
   auto start_mutex = std::chrono::steady_clock::now();
-  std::thread threads[num];
-  for (int i=0; i<num; ++i)
-    threads[i] = std::thread(attempt_10k_increases_mutex);
+  std::vector<std::thread> threads;
+  threads.reserve(num);
+  for (int i=0; i<num; ++i) threads.emplace_back(attempt_10k_increases_mutex);
   for (auto& th : threads) th.join();
   std::cout << "mutex locked version took "
             << p(std::chrono::steady_clock::now() - start_mutex) << "ms\n";
